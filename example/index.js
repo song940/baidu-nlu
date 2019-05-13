ae7240e1da501e53ab383662fb214376eb670b3e
Lsong <song940@gmail.com>
2019-05-13 18:43:06 +0800

    modify

--- example/index.js
+++ example/index.js
@@ -1,13 +1,32 @@
 const BaiduNLU = require('..');
 
 const bot = new BaiduNLU({
-        client_id: 'XUNlxnsSZF7KGGahNbbFGU5j',
-    client_secret: '8s7pi2RfcHKcV7ldUAcsIGZTtn9Hk9b2'
+  client_id: 'XUNlxnsSZF7KGGahNbbFGU5j',
+  client_secret: '8s7pi2RfcHKcV7ldUAcsIGZTtn9Hk9b2'
 });
+
+var postData = {
+  // 'log_id': '7758521',
+  'version': '2.0',
+  'request': {
+    'user_id': '88888',
+    'query_info': {
+      'asr_candidates': [],
+      'type': 'TEXT',
+      'source': 'KEYBOARD'
+    },
+    'bernard_level': 1,
+    // 'updates': '',
+    'query': '你好',
+    // 'client_session': '{"client_results":"", "candidate_options":[]}'
+  },
+  'bot_session': '',
+  'bot_id': '1057'
+};
 
 (async () => {
 
-    const res = await bot.hello();
-    console.log(res);
+  const res = await bot.service(postData);
+  console.log(res);
 
 })();
--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 const xttp = require('xttp');
 
-class BaiduOAuth {
+class BaiduHTTP {
   constructor(options){
     Object.assign(this, options);
   }
+  request(path, params){
+    const { api } = this;
+    return xttp
+      .post(api + path, params)
+      .then(res => res.json());
+  }
+}
+
+class BaiduOAuth extends BaiduHTTP {
   accessToken(grant_type = 'client_credentials'){
     const { client_id, client_secret } = this;
-    return xttp.post('https://aip.baidubce.com/oauth/2.0/token', {
+    return super.request('/oauth/2.0/token', {
       query: {
         grant_type,
         client_id,
         client_secret,
       }
     })
-    .then(res => res.json())
     .then(res => {
       res.scopes = res.scope.split(' ');
       return res;
     });
   }
-  request(){
+  request(path, params){
     return Promise
       .resolve()
       .then(() => this.accessToken())
+      .then(({ access_token }) => {
+        return super.request(path, Object.assign({
+          query: { access_token }
+        }, params));
+      });
   }
 }
 
@@ -31,8 +44,21 @@ class BaiduOAuth {
  * http://ai.baidu.com/docs#/UNIT-v2-API/top
  */
 class BaiduNLU extends BaiduOAuth {
-  hello(){
-    return this.request();
+  constructor(options){
+    super(Object.assign({
+      api: 'https://aip.baidubce.com'
+    }, options));
+  }
+  call(service, params){
+    return this.request(`/rpc/2.0/unit/${service}/chat`, {
+      body: params
+    });
+  }
+  service(params){
+    return this.call('service', params);
+  }
+  bot(params){
+    return this.call('bot', params);
   }
 }
 
